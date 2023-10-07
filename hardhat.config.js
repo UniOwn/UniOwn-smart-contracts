8f6f4d0fa8e532e4f43575890527cf12ec83122f
urrbn <arsfabricius25@gmail.com>
2023-10-07 19:04:09 +0300

    Create hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers")
+require('@openzeppelin/hardhat-upgrades');
+require("@nomiclabs/hardhat-solhint");
+require("hardhat-contract-sizer");
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20", settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.4", settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat:{
+      forking: {
+        url: "https://rpc.ankr.com/polygon",
+        allowUnlimitedContractSize: true,
+        chainId:37
+      }      
+    },
+    polygon: {
+      url: "https://rpc.ankr.com/polygon",
+      accounts:[process.env.PTCHR_PRIVATE_KEY],
+      allowUnlimitedContractSize: true
+    },
+    mumbai: {
+      url: "https://rpc.ankr.com/polygon_mumbai",
+      accounts:[process.env.PTCHR_PRIVATE_KEY],
+      allowUnlimitedContractSize: true
+    }
+  }, 
+  etherscan: {
+    apiKey: process.env.BLOCK_EXPLORER_API_KEY,
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 100,
+    enabled: true
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+};
